8f6ac7d0c25f50f79f525b3833baae9bf0c595fb
Rika <ruijiasun1323@gmail.com>
2018-11-12 15:17:28 -0800

    update webpack config and touch event

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+var path = require('path')
+
 module.exports = {
   entry: "./lib/index.js",
   output: {
-    filename: "./dist/bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js"
   },
   devtool: 'source-map',
 };
